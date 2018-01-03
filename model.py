447b10c7351329bd8e9ad53d58516772802f7a3b
Jian Zhang <quartzing0@gmail.com>
2018-01-03 09:52:32 -0500

    submission

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ for line in lines:
     cnt += 1
     if cnt == 0:
         continue
-    for i in range(3):
+    for i in range(1):
         source_path = line[i]
         filename = source_path.split('/')[-1]
         current_path = 'download/data/IMG/'+filename
